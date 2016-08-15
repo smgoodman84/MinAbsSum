22a5c3d935ec5250e789a41de2fdde6c61b6c435
Simon Goodman <simon.goodman84@googlemail.com>
2016-08-15 18:48:50 +0100

    Add project files.

---
+++ MinAbsSum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinAbsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
