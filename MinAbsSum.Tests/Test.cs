a539fe90441dcc7b4af6ddb4415049a730a1aa15
Simon Goodman <simon.goodman84@googlemail.com>
2016-08-20 11:26:53 +0100

    First Attempt

---
+++ MinAbsSum.Tests/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace MinAbsSum.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Example()
+        {
+            var array = new[] { 1, 5, 2, -2 };
+            var result = new Solution().solution(array);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Failed1()
+        {
+            var array = new[] { 3, 3, 3, 4, 5 };
+            var result = new Solution().solution(array);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ MinAbsSum/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MinAbsSum
+{
+    public class Solution
+    {
+        // https://codility.com/programmers/task/min_abs_sum/
+
+        public int solution(int[] A)
+        {
+            var ordered = A.OrderByDescending(Math.Abs);
+
+            var sum = 0;
+            foreach (var e in ordered)
+            {
+                var multiplier = sum > 0 == e > 0 ? -1 : 1;
+                sum += e * multiplier;
+            }
+
+            return sum;
+        }
+    }
+}
